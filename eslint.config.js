2ace435c4a73d633090a9bb6eca0ff92a8190fe1
AlejandraSaldana <A00838889@tec.mx>
2026-04-15 13:14:14 -0600

    develop 5

--- eslint.config.js
+++ eslint.config.js
@@ -22,6 +22,7 @@ export default defineConfig([
     rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     }
   },
 ])
