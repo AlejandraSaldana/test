d3fd317d2a46cad3f9195ac2c99513d158d5cb8d
AlejandraSaldana <A00838889@tec.mx>
2026-04-15 13:19:23 -0600

    develop 5

--- eslint.config.js
+++ eslint.config.js
@@ -19,10 +19,18 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh
+    },
     rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unsafe-argument': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true
+        }
+      ]
     }
   },
 ])
